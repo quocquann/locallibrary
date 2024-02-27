4229829b5447b8d6fa8647465b6f72c63debf3e7
quocquann <nguyenquocquan2233@gmail.com>
2024-02-27 09:56:07 +0700

    crawl books

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/quocquann/locallibrary
 
 go 1.22.0
+
+require github.com/PuerkitoBio/goquery v1.9.0
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/quocquann/locallibrary/models"
+)
+
+func crawlBook(url string) ([]models.Book, error) {
+	books := []models.Book{}
+	res, err := http.Get(url)
+	if err != nil {
+		return []models.Book{}, err
+	}
+
+	if res.StatusCode != 200 {
+		return []models.Book{}, fmt.Errorf("status code error: %d", res.StatusCode)
+	}
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+
+	if err != nil {
+		return []models.Book{}, err
+	}
+
+	doc.Find(".product-loop-1.product-loop-sea.product-base").Each(func(i int, s *goquery.Selection) {
+		title := s.Find("h3.product-name a").Text()
+		book := models.Book{Title: title}
+		books = append(books, book)
+	})
+
+	return books, nil
+}
 
 func main() {
-	fmt.Println("running")
+	books, err := crawlBook("https://gacxepbookstore.vn/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(books)
 }
